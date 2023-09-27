7cc9a02160d4a50ee770eda795ba5f4d9a963190
cr <cswd@yandex.ru>
2023-09-28 01:07:28 +0700

    Добавлен класс кеширования, позволяющий хранить результаты вызова функций чтобы не проводить рассчёты заново.

---
+++ src/lab1/graphics/Lazy.java
@@ -0,0 +1,19 @@
+package lab1.graphics;
+
+public class Lazy<T> {
+    private T cached = null;
+    private boolean produced = false;
+    private final Producer<T> producer;
+
+    public Lazy(Producer<T> producer){
+        this.producer = producer;
+    }
+    public T get(){
+        if (produced)
+            return cached;
+        produced = true;
+        cached = producer.invoke();
+        return cached;
+    }
+
+}
