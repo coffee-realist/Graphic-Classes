92df7b3347a05600f8cb9f3e02d5039a2de12d39
cr <cswd@yandex.ru>
2023-09-28 01:01:18 +0700

    Изменена логика пересчёта и хранения площади при помощи кеширования. Убраны не использующиеся методы. Убран метод toString(). Логика метода expandTo переделана для соответствия критериям лабораторной работы.

--- src/lab1/graphics/Figure.java
+++ src/lab1/graphics/Figure.java
@@ -1,24 +1,15 @@
 package lab1.graphics;
 
 public abstract class Figure extends Drawable {
-    private double square;
+    protected Lazy<Double> square;
 
-    abstract public void calculateSquare();
-
-    public void setSquare(double square){
-        this.square = square;
-    }
+    abstract protected double calculateSquare();
 
     public double getSquare(){
-        return square;
+        return square.get();
     }
-
-    abstract public void expandTo(double multiplier);
+    abstract public Ellipse getCircumscribedCircle();
+    abstract public Figure expandTo(double multiplier);
     abstract public void move(double delta_x, double delta_y);
-
-    @Override
-    public String toString() {
-        return "Класс геометрических фигур, имеющих площадь.";
-    }
 }
 
