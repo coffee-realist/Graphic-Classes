583d1900034963d8df200b551cdc48e3503ebb42
cr <cswd@yandex.ru>
2023-10-02 14:48:21 +0700

    Метод move теперь соответствует требованиям. Изменена логика кеширования площади и убран лишний метод. Введены вычисления с использованием добавленной векторной геометрии точек. Откорректирован метод toString(). Некоторые из параметров класса стали локальными переменными конструктора.

--- src/lab1/graphics/Rectangle.java
+++ src/lab1/graphics/Rectangle.java
@@ -8,54 +8,38 @@ public class Rectangle extends Figure {
     private final Dot dot3;
     private final Dot dot4;
     private final double rotation;
-    private final double width;
-    private final double height;
     private final double diagonal;
     private final Dot center;
 
     public Rectangle(Dot dot1, Dot dot2, Dot dot3, Dot dot4) {
-        double width = Math.sqrt(Math.pow(dot1.getX() - dot4.getX(), 2) + Math.pow(dot1.getY() - dot4.getY(), 2));
-        double height = Math.sqrt(Math.pow(dot1.getX() - dot2.getX(), 2) + Math.pow(dot1.getY() - dot4.getY(), 2));
+        super(() -> dot2.length(dot1) * dot4.length(dot1));
+        double width = dot2.length(dot1);
+        double height = dot4.length(dot1);
         check(dot1, dot2, dot3, dot4, width, height);
         this.dot1 = dot1;
         this.dot2 = dot2;
         this.dot3 = dot3;
         this.dot4 = dot4;
-        this.width = width;
-        this.height = height;
-        double rotation = -Math.acos((dot4.getX() - dot1.getX()) / width);
+        double rotation = -Math.acos((dot4.minus(dot1).scalarProduct(new Dot(1, 0)) / width));
         if (rotation < 0)
             rotation += 360;
         this.rotation = rotation;
-        this.center = new Dot((dot3.getX() - dot1.getX()) / 2 + dot1.getX(),
-                (dot3.getY() - dot1.getY()) / 2 + dot1.getY());
-        this.diagonal = Math.sqrt(Math.pow(dot3.getX() - dot1.getX(), 2) + Math.pow(dot3.getY() - dot1.getY(), 2));
-        square = new Lazy<>(this::calculateSquare);
+        this.center = dot3.minus(dot1).product(0.5).plus(dot1);
+        this.diagonal = dot3.length(dot1);
     }
 
-    @Override
-    public double calculateSquare() {
-        return height * width;
-    }
 
-    public void check(Dot d1, Dot d2, Dot d3, Dot d4, double width, double height) {
-        if (d1.getX() < 0 || d1.getY() < 0 || d2.getX() < 0 || d2.getY() < 0 ||
-                d3.getX() < 0 || d3.getY() < 0 || d4.getX() < 0 || d4.getY() < 0)
-            throw new RuntimeException("Координаты не могут быть отрицательными. Пожалуйста задайте другие координаты");
-        if ((!(d2.getX() - d1.getX() == d3.getX() - d4.getX() &&
-                d2.getY() - d1.getY() == d3.getY() - d4.getY() &&
-                d3.getX() - d2.getX() == d4.getX() - d1.getX() &&
-                d3.getY() - d2.getY() == d4.getY() - d1.getY())) ||
-                (width == 0) || (height == 0))
+
+    private void check(Dot d1, Dot d2, Dot d3, Dot d4, double width, double height) {
+        if ((!(Double.compare(d1.length(d2), d3.length(d4)) == 0 && Double.compare(d2.length(d3), d1.length(d4)) == 0)) ||
+                Double.compare(width, 0) == 0 || Double.compare(height, 0) == 0 || !(Double.compare(d2.minus(d1).scalarProduct(d4.minus(d1)), 0) == 0))
             throw new RuntimeException("Это не прямоугольник. Пожалуйста задайте другие координаты");
     }
 
     @Override
-    public void move(double delta_x, double delta_y) {
-        dot1.move(delta_x, delta_y);
-        dot2.move(delta_x, delta_y);
-        dot3.move(delta_x, delta_y);
-        dot4.move(delta_x, delta_y);
+    public Rectangle move(double delta_x, double delta_y) {
+        return new Rectangle(dot1.move(delta_x, delta_y), dot2.move(delta_x, delta_y),
+                dot3.move(delta_x, delta_y), dot4.move(delta_x, delta_y));
     }
 
     public Ellipse getCircumscribedCircle() {
@@ -64,24 +48,19 @@ public class Rectangle extends Figure {
 
     @Override
     public Rectangle expandTo(double multiplier) {
-        return new Rectangle(new Dot((dot1.getX() - center.getX()) * multiplier,
-                (dot1.getY() - center.getY()) * multiplier),
-                new Dot((dot2.getX() - center.getX()) * multiplier,
-                (dot2.getY() - center.getY()) * multiplier),
-                new Dot((dot3.getX() - center.getX()) * multiplier,
-                (dot3.getY() - center.getY()) * multiplier),
-                new Dot((dot4.getX() - center.getX()) * multiplier,
-                (dot4.getY() - center.getY()) * multiplier));
+        return new Rectangle(dot1.minus(center).product(multiplier),
+                dot2.minus(center).product(multiplier),
+                dot3.minus(center).product(multiplier),
+                dot4.minus(center).product(multiplier));
     }
 
     @Override
     public String toString() {
         return String.format("""
-                        Прямоугольник.
-                        Координаты точек:
-                        %s%s%s%sПлощадь: %f
-                        """, dot1.toString(), dot2.toString(),
-                dot3.toString(), dot4.toString(), getSquare());
+                Прямоугольник.
+                Координаты точек:
+                %s%s%s%sПлощадь: %f
+                """, dot1, dot2, dot3, dot4, getSquare());
     }
 
     @Override
